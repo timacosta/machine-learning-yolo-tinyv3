b9b54073a8d27f64745559dcad8f5f5ee5462c96
Tim Acosta <timacosta06@gmail.com>
2021-04-17 21:53:18 +0100

    Initial Commit

---
+++ machine-learning-yolo-tinyv3/CatalogueViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  machine-learning-yolo-tinyv3
+//
+//  Created by Tim Acosta on 16/4/21.
+//
+
+import UIKit
+
+class CatalogueViewController: UICollectionViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    collectionView.backgroundColor = .blue
+    
+    
+  }
+
+
+}
+
